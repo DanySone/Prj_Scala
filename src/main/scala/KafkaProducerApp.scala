cb88e26eb059fc5522dc67a7f28ad9ebc0a25453
DanySone <62615122+DanySone@users.noreply.github.com>
2021-04-17 15:12:49 +0200

    17/04 add recursive drone report

--- src/main/scala/KafkaProducerApp.scala
+++ src/main/scala/KafkaProducerApp.scala
@@ -3,6 +3,8 @@ import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 import java.util.Properties
 import scala.concurrent.duration.{FiniteDuration, SECONDS}
 import List._
+import org.apache.kafka.clients.producer.ProducerConfig
+
 
 object KafkaProducerApp extends App {
 
@@ -15,31 +17,37 @@ object KafkaProducerApp extends App {
     "org.apache.kafka.common.serialization.StringSerializer")
   props.put("acks","all")
 
+
+
+
   //Instantiate producer
   val producer = new KafkaProducer[String, String](props)
 
   // Name of the topic that will be used to put messages
   val topic = "text_topic"
 
+  def report_generator(p: KafkaProducer[String, String]) {
+    val d1 = DroneReportObj.randrep()
+    val report = new ProducerRecord[String, String](topic,
+      d1._id.toString + ";"
+        + d1._latitude + ";"
+        + d1._longitude + ";"
+        + d1._surrounding.toString + ";"
+        + d1._words.toString + ";"
+        + d1._day)
+    val metadata = p.send(report)
+    Thread.sleep(5000L)
+    report_generator(p)
+  }
   try {
-    fill(50) {
-      val d1 = DroneReportObj.randrep()
-      val alert = d1._surrounding.filter((t) => t._2 < 50)
-      if (alert.size == 1)
-        println("Alert ! the citizen " + alert + "is unhappy, he must go to the peaceland prison.")
-      else (alert.size > 1)
-        println("Alert ! the citizens " + alert + "are unhappy, they must go to the peaceland prison.")
-      val report = new ProducerRecord[String, String](topic,
-        d1._id.toString + ";"
-          + d1._latitude + ";"
-          + d1._longitude + ";"
-          + d1._surrounding.toString + ";"
-          + d1._words.toString + ";"
-          + d1._day)
-      producer.send(report)
-      Thread.sleep(60000L)
-    }
+    val d1 = DroneReportObj.randrep()
+    val alert = d1._surrounding.filter((t) => t._2 < 50)
 
+    if (alert.size == 1)
+      println("Alert ! the citizen " + alert + " is unhappy, he must go to the peaceland prison.")
+    else (alert.size > 1)
+    println("Alert ! the citizens " + alert + " are unhappy, they must go to the peaceland prison.")
+    report_generator(producer)
   }
   catch {
     case e:Exception => e.printStackTrace()
